a3332f80cf9ad5d52a93c90e55db2b6678f12384
Caleb York <caleb.york@fwcsstudents.org>
2025-04-30 10:24:45 -0400

    Complete TIY 19-1

--- blogs/urls.py
+++ blogs/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path("new_blog/", views.new_blog, name="new_blog"),
     # Path to add a new blog post.
     path("new_post/<int:blog_id>", views.new_post, name="new_post"),
+    # Path to edit a blog post.
+    path("edit_post/<int:post_id>", views.edit_post, name="edit_post"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Blog
+from .models import Blog, BlogPost
 from .forms import BlogForm, BlogPostForm
 
 
@@ -58,3 +58,22 @@ def new_post(request, blog_id):
 
     context = {"blog": blog, "form": form}
     return render(request, "blogs/new_post.html", context)
+
+
+def edit_post(request, post_id):
+    """Edit an existing post."""
+    post = BlogPost.objects.get(id=post_id)
+    blog = post.blog
+
+    if request.method != "POST":
+        # Fill the form with the existing information.
+        form = BlogPostForm(instance=post)
+    else:
+        # Submit data.
+        form = BlogPostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("blogs:blog", blog_id=blog.id)
+
+    context = {"post": post, "blog": blog, "form": form}
+    return render(request, "blogs/edit_post.html", context)
